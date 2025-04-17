261e8f1a138e878d279509a8d69a40bac5630371
Oliver Peterson <156498600+opeters1@users.noreply.github.com>
2025-04-17 10:08:44 -0700

    Update PhotoresistorNightlight.ino

--- PhotoresistorNightlight.ino
+++ PhotoresistorNightlight.ino
@@ -4,17 +4,15 @@ int sensor;
 const int threshold = 500;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(led, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   sensor = analogRead(sensor_pin);
   Serial.println(sensor);
   delay(500);
-  if (sensor<threshold) {
+  if (sensor<threshold) { /* test if lights are on */
     digitalWrite(led, HIGH);
   }
   else {
